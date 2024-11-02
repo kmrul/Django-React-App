f4f1cdba383044f04377e0e24de474730ddd13b0
kamrul <kamruldevcs@gmail.com>
2024-11-02 23:59:42 +0600

    [ADD] connect with backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,6 +7,8 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl
 })
 
+console.log("baseURL:",baseURL);
+
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem(ACCESS_TOKEN);
